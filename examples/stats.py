9a34ab0af26ab476c9c8f8316def8c0b8674e0bf
Ivaylo Iliev <i_z_iliev@yahoo.com>
2023-10-14 23:16:49 +0300

    Update stats.py

--- examples/stats.py
+++ examples/stats.py
@@ -44,10 +44,10 @@ SPI_DEVICE = 0
 # SPI_DEVICE = 0
 
 # 128x32 display with hardware I2C:
-disp = Adafruit_SSD1306.SSD1306_128_32(rst=RST)
+# disp = Adafruit_SSD1306.SSD1306_128_32(rst=RST)
 
 # 128x64 display with hardware I2C:
-# disp = Adafruit_SSD1306.SSD1306_128_64(rst=RST)
+disp = Adafruit_SSD1306.SSD1306_128_64(rst=RST)
 
 # Note you can change the I2C address by passing an i2c_address parameter like:
 # disp = Adafruit_SSD1306.SSD1306_128_64(rst=RST, i2c_address=0x3C)
@@ -96,11 +96,12 @@ x = 0
 
 
 # Load default font.
-font = ImageFont.load_default()
+# font = ImageFont.load_default()
 
 # Alternatively load a TTF font.  Make sure the .ttf font file is in the same directory as the python script!
 # Some other nice fonts to try: http://www.dafont.com/bitmap.php
 # font = ImageFont.truetype('Minecraftia.ttf', 8)
+font = ImageFont.truetype('PixelOperator.ttf', 16)
 
 while True:
 
@@ -108,24 +109,25 @@ while True:
     draw.rectangle((0,0,width,height), outline=0, fill=0)
 
     # Shell scripts for system monitoring from here : https://unix.stackexchange.com/questions/119126/command-to-display-memory-usage-disk-usage-and-cpu-load
-    cmd = "hostname -I |cut -f 2 -d ' '"
+    cmd = "hostname -I | cut -d\' \' -f1"
     IP = subprocess.check_output(cmd, shell = True )
     cmd = "top -bn1 | grep load | awk '{printf \"CPU Load: %.2f\", $(NF-2)}'"
     CPU = subprocess.check_output(cmd, shell = True )
     cmd = "free -m | awk 'NR==2{printf \"Mem: %s/%sMB %.2f%%\", $3,$2,$3*100/$2 }'"
     MemUsage = subprocess.check_output(cmd, shell = True )
-    #cmd = "df -h | awk '$NF==\"/\"{printf \"Disk: %d/%dGB %s\", $3,$2,$5}'"
     cmd = "df -h | awk '/NASHDD1/ {printf \"Disk: %d/%dGB %s\", $3,$2,$5}'"
     Disk = subprocess.check_output(cmd, shell = True )
     cmd = "vcgencmd measure_temp |cut -f 2 -d '='"
     temp = subprocess.check_output(cmd, shell = True )
 
-    # Write two lines of text.
 
-    draw.text((x, top), "IP: " + str(IP,'utf-8'), font=font, fill=255)
-    draw.text((x, top+8), str(CPU,'utf-8') + " " + str(temp,'utf-8') , font=font, fill=255)
-    draw.text((x, top+16), str(MemUsage,'utf-8'), font=font, fill=255)
-    draw.text((x, top+25), str(Disk,'utf-8'), font=font, fill=255)
+    # Write two lines of text.
+    
+    draw.text((x, top+2), "IP: " + str(IP,'utf-8'), font=font, fill=255)
+    draw.text((x, top+18), str(CPU,'utf-8') + " " + str(temp,'utf-8') , font=font, fill=255)
+    draw.text((x, top+34), str(MemUsage,'utf-8'), font=font, fill=255)
+    draw.text((x, top+50), str(Disk,'utf-8'), font=font, fill=255)
+    
 
     # Display image.
     disp.image(image)
